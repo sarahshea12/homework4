7d1925f9dd38c0737fb417d3533c72e28849e6d0
Sarah Shealy <sshealy124@gmail.com>
2020-05-15 00:49:09 -0400

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+// Click button event: hide start page, show q1, run program 
+
+// Timer
+
+// if wrong, subtract 10 from timer
+
+// keep score; for right answers add points
+
+// add display for high score
