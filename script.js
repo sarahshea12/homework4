0708c622d1243cada5a5a8f8b22dc20087f5a6ec
Sarah Shealy <sshealy124@gmail.com>
2020-05-16 00:28:44 -0400

    updates

--- script.js
+++ script.js
@@ -1,9 +1,74 @@
-// Click button event: hide start page, show q1, run program 
+var beginbutton = document.getElementById("begin")
+var q1 = document.getElementById("question1")
+var q2 = document.getElementById("question2")
+var q3 = document.getElementById("question3")
+var secs = document.getElementById("seconds")
+var head = document.getElementById("title")
+var score = 0;
+var timeLeft = 30;
 
-// Timer
+//start timer
+beginbutton.addEventListener("click", function startTimer() {
+    
+    var timeInterval = setInterval(function() {
+      secs.textContent = timeLeft;
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        secs.textContent = "game over";
+        clearInterval(timeInterval);
+      }
+  
+    }, 1000);
+  })
+  
+//hide and show questions
+beginbutton.addEventListener("click", function() {
+    //q1.classList.remove("hide");
+    q1.removeAttribute("class","hide");
+    head.classList.add("hide")
+    beginbutton.classList.add("hide")
+})
 
-// if wrong, subtract 10 from timer
+// if wrong, subtract 5 from timer; if right, add 100 to score
+var correct1 = document.getElementById("right1")
+var correct2 = document.getElementById("right2")
+var correct3 = document.getElementById("right3")
 
-// keep score; for right answers add points
+correct1.addEventListener("click", function() {
+    score = score + 100
+    console.log(score)
+})
+correct2.addEventListener("click", function() {
+    score = score + 100
+    console.log(score)
+})
+correct3.addEventListener("click", function() {
+    score = score + 100
+    console.log(score)
+})
 
-// add display for high score
+// append high scores
+var uInput = document.getElementById("text").value;
+var names = [""];
+names.push(uInput + score);
+
+for(var i=0; i<name.length; i++){
+    console.log(names[i]);
+    var li = document.createElement("<li>");
+    li.text(names[i]);
+    document.getElementById("names").append(li);
+}
+
+
+
+
+
+
+
+// function() {
+//for(i=0;i<10;i++){
+//    a = a + i
+//}
+//return a;
+//
